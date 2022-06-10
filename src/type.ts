ad42a9a99146ab4137fbafaf2867be08dbba921a
winixt <haizekuo@gmail.com>
2022-06-10 20:52:30 +0800

    feat: 添加 component source type

--- src/type.ts
+++ src/type.ts
@@ -1,3 +1,11 @@
 export interface Schema {
     version: string;
+    componentsMap: ComponentSource[];
+}
+
+export interface ComponentSource {
+    componentName: string;
+    package: string;
+    version?: string;
+    exportName?: string;
 }
