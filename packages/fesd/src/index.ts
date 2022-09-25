8780ce53835d55ec57e753d90892d54913c563f8
winixt <haizekuo@gmail.com>
2022-09-25 19:47:12 +0800

    feat: fesd 添加 core 的导出“

--- packages/fesd/src/index.ts
+++ packages/fesd/src/index.ts
@@ -3,6 +3,8 @@ import { BlockSchema } from './type';
 import { genBlockSchema } from './genPage/list';
 import { createContext } from './context';
 
+export * from '@qlin/toycode-core';
+
 // FEATURE 后续根据情况优化命名，不能以页面类型为维度
 export function genListPageCode(blockSchema: BlockSchema, config: Config = {}) {
     const ctx = createContext();
